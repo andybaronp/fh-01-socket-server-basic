0dc57db7d13463d494a6cee54f6d632ba97ec579
Jhoandris B <andybaronp@gmail.com>
2023-10-31 11:25:49 -0500

    inicio min chat

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ const io = require('socket.io')(server);
 //desplegar el directorio public
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', () => {
-  console.log('new connection');
+io.on('connection', (socket) => {
+  socket.emit('welcome', {
+    message: 'Welcome to the chat',
+    id: socket.id
+  });
+
+  socket.on('message-to-server', (data) => {
+    io.emit('messages', data)
+  });
 });
 server.listen(3002, () => {
   console.log('listening on 3002');
